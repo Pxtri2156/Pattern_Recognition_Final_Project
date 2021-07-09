977e0faaf96f808dacffab433e2f24b4fa72eb8d
= <=>
2021-07-09 11:41:50 +0700

    Update train CNN

---
+++ libs/CNN/CNN_class.py
@@ -0,0 +1,134 @@
+import os 
+import librosa                                                                                                                                                                                                                                                                                                                                                             
+import argparse
+import numpy as np 
+import sys
+import joblib
+import json
+from tqdm import tqdm
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import svm
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+import tensorflow.keras
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Input, Flatten, Dropout, Activation
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.metrics import confusion_matrix
+from tensorflow.keras.models import model_from_json
+
+
+import warnings
+sys.path.append("./")
+from utils.loading import load_features_labels
+from utils.config_parse import get_config
+from utils.write_log import write_config_args_2_log
+from utils.setup_log import setup_logging
+import logging
+
+class CNN:
+
+    def __init__(self, cfgs):
+        self.log_train = None
+        self.resume = False
+        print("Called init function")
+        self.model = None
+        self.input_size = cfgs.CNN.MODEL.INPUT_SIZE
+        self.output_size = cfgs.CNN.MODEL.OUTPUT_SIZE
+        self.epochs = cfgs.CNN.MODEL.EPOCHS
+        self.batch_size = cfgs.CNN.MODEL.BATCH_SIZE
+        self.define_model()
+
+    def define_model(self):
+
+        # Define model
+        model = Sequential()
+        model.add(Conv1D(256, 5,padding='same',
+                        input_shape=(self.input_size,1)))
+        model.add(Activation('relu'))
+        model.add(Conv1D(128, 5,padding='same'))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.1))
+        model.add(MaxPooling1D(pool_size=(8)))
+        model.add(Conv1D(128, 5,padding='same',))
+        model.add(Activation('relu'))
+        #model.add(Conv1D(128, 5,padding='same',))
+        #model.add(Activation('relu'))
+        #model.add(Conv1D(128, 5,padding='same',))
+        #model.add(Activation('relu'))
+        #model.add(Dropout(0.2))
+        model.add(Conv1D(128, 5,padding='same',))
+        model.add(Activation('relu'))
+        model.add(Flatten())
+        model.add(Dense(self.output_size))
+        model.add(Activation('softmax'))
+        self.model = model
+
+    def print_architecture_model():
+        self.model.summary()
+
+    def train(self,x_traincnn, x_testcnn, y_train, y_test):
+        assert(size(x_traincnn.shape) == 3 ), "x_traincnn must be tensor (dimenson = 3)"
+        assert(size(x_testcnn.shape) == 3 ), "x_testcnn must be tensor (dimenson = 3)"
+        
+        # set optimize 
+        opt = tensorflow.keras.optimizers.RMSprop(lr=0.00001, decay=1e-6)
+        self.model.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'])
+        self.log_train = self.model.self.model.fit(x_traincnn, y_train, batch_size=self.batch_size, epochs=self.epochs, validation_data=(x_testcnn, y_test))
+        
+    def predict(self):
+        pass
+    
+    def plot_log(self, plot_file):
+        plt.plot(self.log_train.history['loss'])
+        plt.plot(self.log_train.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+    
+    def save_architecture_model(self, json_path):
+        model_json = self.model.to_json()
+        with open(json_path, "w") as json_file:
+            json_file.write(model_json)
+
+    def save_weight_model(self, model_path):
+        model_name = 'Emotion_Voice_Detection_Model.h5'
+        if model_path == "":
+            save_dir = os.path.join(os.getcwd(), 'saved_models')
+        else:
+            save_dir = model_path
+        # Save model and weights
+        if not os.path.isdir(save_dir):
+            os.makedirs(save_dir)
+        model_path = os.path.join(save_dir, model_name)
+        self.model.save(model_path)
+        print('Saved trained model at %s ' % model_path)
+
+    def load_architecture_model(self, input_path):
+        '''
+            input_path: json file
+        '''
+        assert(os.path.isfile(input_path)),"Model architecture path not exist or wrong!!!"
+        json_file = open(input_path, 'r')
+        loaded_model_json = json_file.read()
+        loaded_model = model_from_json(loaded_model_json)
+        self.model = model 
+        json_file.close()
+
+    def load_weight_model(self, model_path):
+        assert(os.path.isfile(model_path)),"Model weight path not exist or wrong!!!"
+        self.model.load_weights(model_path)
--- libs/CNN/train.py
+++ libs/CNN/train.py
@@ -14,20 +14,22 @@ from sklearn import svm
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-
-import keras
-from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Embedding
-from keras.layers import LSTM
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-from keras.layers import Input, Flatten, Dropout, Activation
-from keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
-from keras.models import Model
-from keras.callbacks import ModelCheckpoint
 from sklearn.metrics import confusion_matrix
+
+
+import tensorflow.keras
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Input, Flatten, Dropout, Activation
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.utils import to_categorical
 
 import warnings
 sys.path.append("./")
@@ -35,6 +37,7 @@ from utils.loading import load_features_labels
 from utils.config_parse import get_config
 from utils.write_log import write_config_args_2_log
 from utils.setup_log import setup_logging
+from libs.CNN.CNN_class import CNN
 import logging
 
 
@@ -45,45 +48,65 @@ def evaluate_model(y_true, y_pred, tar_names):
     logger.info("{} RESULT EVALUATION {} \n {}".format('_'*30, "_"*30, cls_report ))
 
 
-def train(args, cfgs):
+def train(cfgs):
 
     # Load feature data
-    print("[INFO]: Step 1/7 - Loading features and labels]")
-    train_features, train_labels, dev_features, dev_labels = load_features_labels(args.root, args.data_n)
-    # print('features: ', features)
-    # print("labels: ", labels)
-    print("[INFO]: Step 2/7 - Preprocessing labels]")
+    print("[INFO]: Step 1/8 - Loading features and labels]")
+    train_features, train_labels, dev_features, dev_labels = load_features_labels(cfgs.CNN.ROOT, cfgs.CNN.DATASET_NAME)
+    # print('features: ', features) 
+    # print("labels: ", labels) 
+    print("[INFO]: Step 2/8 - Preprocessing labels]")
     print('features shape: ', train_features.shape) 
     le = preprocessing.LabelEncoder()
     train_labels = le.fit_transform(train_labels)
     cls_n = le.classes_
-    print("class: ", cls_n)
+    print("class: ", cls_n) 
     # train_labels = train_labels[:24] # Must to comment
     print('len of train labels: ', len(train_labels))
     # Split data 
-    print("[INFO]: Step 3/7 Split data ")
+    print("[INFO]: Step 3/8 Split data ")
     if dev_features == []:
-        print("[INFO]:  Split data with train - val : {}-{}".format( 1- cfgs.KNN.SPLIT, cfgs.KNN.SPLIT ))
-        X_train , X_val, Y_train, Y_val = train_test_split(train_features, train_labels, test_size= cfgs.KNN.SPLIT, shuffle=True)
+        print("[INFO]:  Split data with train - val : {}-{}".format( 1- cfgs.CNN.SPLIT, cfgs.CNN.SPLIT ))
+        X_train , X_val, Y_train, Y_val = train_test_split(train_features, train_labels, test_size= cfgs.CNN.SPLIT, shuffle=True)
     else:
         print("[INFO]: Loading dev path")
         dev_labels = le.fit_transform(dev_labels)
         X_train, Y_train = train_features, train_labels
         X_val, Y_val = dev_features, dev_labels
-
+    print("Before fix for CNN")
     print("X train shape: ", X_train.shape)
     print('X val shape: ', X_val.shape) 
-    print('Y train',  Y_train)
     print("Y train shape: ", len(Y_train))
     print("Y val: " , len(Y_val))
+
+    print("[INFO]: Step 4/8 fix label and feature for CNN")
+    # fix label for CNN 
+    # Y_train = np_utils.to_categorical(Y_train)
+    # Y_val = np_utils.to_categorical(Y_val)
+    Y_train = to_categorical(Y_train)
+    Y_val = to_categorical(Y_val)
+
+    # Changing dimension for CNN model
+    X_train =np.expand_dims(X_train, axis=2)
+    X_val= np.expand_dims(X_val, axis=2)
+
+    print("After fix for CNN")
+    print("X train shape: ", X_train.shape)
+    print('X val shape: ', X_val.shape) 
+    print("Y train shape: ", Y_train.shape)
+    print("Y val: " , Y_val.shape)
+
     # Define model
-    print("[INFO]: Step 4/7 Define model KNeighborsClassifier ")
-    model = KNeighborsClassifier(n_neighbors = cfgs.KNN.K )
+    print("[INFO]: Step 5/8 Define model CNN ")
+    model = CNN(cfgs)
+    # Print architecture model
+    model.print_architecture_model()
     # Training
-    print("[INFO]: Step 5/7 - Training ")
-    model.fit(X_train , Y_train )
+    print("[INFO]: Step 6/8 - Training ")
+    model.train(X_train, X_val,  Y_train, Y_val)
     print("[INFO]: Completed training")
-    print("[INFO]: Step 6/7 - Evaluation on validation set")
+    print("[INFO]: Step 7/8 - Evaluation on validation set")
+    input("Stop here")
     predicted = model.predict(X_val)
     # Y_val = [1, 2, 7, 3, 0,4,5, 6,6]
     # predicted = [0, 0 ,0 ,0, 0, 0, 0, 1,2]
@@ -92,18 +115,18 @@ def train(args, cfgs):
     evaluate_model(Y_val, predicted, cls_n)
     # print("predicted: ", le.inverse_transform(predicted))
     # Save model
-    print("[INFO]: 6/6 - Saving model")
+    print("[INFO]: 8/8 - Saving model")
     logging.info("Model was trained")
     save_path = os.path.join( args.output, 'model.pkl')
     joblib.dump(model, save_path)
 
 
-def main(args, cfgs):
+def main(cfgs):
     global logger 
     logger = setup_logging(os.path.join(args.output, 'log.log'))
     logger.disabled = False;
-    write_config_args_2_log(cfgs, args, logger)
-    train(args, cfgs)
+    # write_config_args_2_log(cfgs, args, logger)
+    train(cfgs)
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='Argument of extract data')
@@ -111,53 +134,96 @@ def arg_parser():
                         type=str, help='The path consists all type extracted features')
     parser.add_argument('--data_n', '-n', default='all', 
                         type=str, help='name of dataset you want to train')
-    parser.add_argument('--features', '-f', default="all", 
-                    type=str, help='The type method that you want to extract')
+    # parser.add_argument('--features', '-f', default="all", 
+    #                 type=str, help='The type method that you want to extract')
     parser.add_argument('--argu', '-a', action='store_true', 
                     help='Augment data')
-    parser.add_argument('--output', '-o', required=True, 
+    parser.add_argument('--weight_model', '-o', required=True, 
+                        type=str, help='The path will save model')
+    parser.add_argument('--architecture_model', '-ar', default="", 
+                        type=str, help='The path will save model')
+    parser.add_argument('--visualize', '-v', default="", 
                         type=str, help='The path will save model')
     parser.add_argument('--configs_file', '-c', required=True, 
                         type=str, help='The path config file for model')
+    parser.add_argument(
+    "--opts",
+    help="Argument of Dict guided method, modify config options using the command-line 'KEY VALUE' pairs",
+    default=[],
+    nargs=argparse.REMAINDER,
+    )
+
     return parser.parse_args()
 
-def print_args_cfg(args, cfgs):
-    print("{} ARGUMENT {}".format('_'*30, "_"*30))
+def print_args(args):
+    print("{} PARSER {}".format('_'*30, "_"*30))
     print("{} The path root of exacted features: \n\t{}".format('#'*3, args.root))
     print("{} Dataset name to train: \n\t{}".format('#'*3, args.data_n))
-    print("{} The methods exacted feature: \n\t{}".format('#'*3, args.features))
+    # print("{} The methods exacted feature: \n\t{}".format('#'*3, args.features))
     print("{} Augment data: \n\t{}".format('#'*3, args.argu))
     print("{} Dataset name to train: \n\t{}".format('#'*3, args.data_n))
-    print("{} The path of model ouput : \n\t{}".format('#'*3, args.output))
+    print("{} The path of weight model : \n\t{}".format('#'*3, args.weight_model))
+    print("{} The path of architecture model : \n\t{}".format('#'*3, args.architecture_model))
+    print("{} The path of visualize : \n\t{}".format('#'*3, args.visualize))
     print("{} The path of configs file : \n\t{}".format('#'*3, args.configs_file))
 
+def print_cfgs(cfgs):
     print("{} CONFIGS {}".format('_'*30, "_"*30))
-    print("{} Split dataset train - val : {}-{}".format('#'*3, 1- cfgs.KNN.SPLIT, cfgs.KNN.SPLIT ))
-    print("{} Number neighbor: {}".format('#'*3, cfgs.KNN.K))
+    print("{} Split dataset train - val : {}-{}".format('\t', 1- cfgs.CNN.SPLIT, cfgs.CNN.SPLIT ))
+    print("{} ROOT: ".format('\t', cfgs.CNN.ROOT ))
+    print("{} DATASET_NAME name: ".format('\t', cfgs.CNN.DATASET_NAME ))
+    print("{} FEATURES: ".format('\t', cfgs.CNN.FEATURES ))
+    print("{} WEIGHTS_PATH: ".format('\t', cfgs.CNN.WEIGHTS_PATH ))
+    print("{} ARCHITECTURE_MODEL_PATH: ".format('\t', cfgs.CNN.ARCHITECTURE_MODEL_PATH ))
+    print("{} VISUALIZE_PATH: ".format('\t', cfgs.CNN.VISUALIZE_PATH ))
+    print("{} MODEL: ".format('\t' ))
+    print("{} INPUT_SIZE: ".format('\t\t', cfgs.CNN.MODEL.INPUT_SIZE))
+    print("{} OUTPUT_SIZE: ".format('\t\t', cfgs.CNN.MODEL.OUTPUT_SIZE))
+    print("{} ACTIVATION: ".format('\t\t', cfgs.CNN.MODEL.ACTIVATION))
+    print("{} PADDING: ".format('\t\t', cfgs.CNN.MODEL.PADDING))
+    print("{} LEARNING_RATE: ".format('\t\t', cfgs.CNN.MODEL.LEARNING_RATE))
+    print("{} DE_CAY: ".format('\t\t', cfgs.CNN.MODEL.DE_CAY))
+    print("{} LOSS_FUNCTION: ".format('\t\t', cfgs.CNN.MODEL.LOSS_FUNCTION))
+    print("{} METRIC: ".format('\t\t', cfgs.CNN.MODEL.METRIC))
+    print("{} BATCH_SIZE: ".format('\t\t', cfgs.CNN.MODEL.BATCH_SIZE))
+    print("{} EPOCHS: ".format('\t\t', cfgs.CNN.MODEL.EPOCHS))
+
     print("{}\n".format('_'*100))
 
+def setup_config(cfgs, args):
+    cfgs.CNN.ROOT = args.root
+    cfgs.CNN.DATASET_NAME  = args.data_n
+    cfgs.ARGUMENT = args.argu
+    cfgs.WEIGHTS_PATH = args.weight_model
+    if args.architecture_model != "":
+        cfgs.ARCHITECTURE_MODEL_PATH = args.architecture_model
+    if args.visualize != "":
+        cfgs.VISUALIZE_PATH = args.visualize
 
+    return cfgs
 if __name__ == "__main__":
     args = arg_parser()
     cfgs = get_config()
-    cfgs.merge_from_file('./configs/BASE/Base.yaml')
+    cfgs.merge_from_file('./configs/CNN/CNN.yaml')
     if not os.path.isfile(args.configs_file):
         print("File configs not exits!!")
     # old_stdout = sys.stdout
     # log_file = open(os.path.join(args.output, 'log.log'),"w")
     # sys.stdout = log_file
     cfgs.merge_from_file(args.configs_file)
-    print_args_cfg(args, cfgs)
-    main(args, cfgs)
+    print_args(args)
+    cfgs = setup_config(cfgs, args)
+    print_cfgs(cfgs)
+    main(cfgs)
     # sys.stdout = old_stdout
-
     # log_file.close()
 
 '''
     -r E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\feature_data \
     -n TESS \
-    -o E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\libs\KNN  \
-    -c E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\configs\KNN\KNN.yaml
+    -o E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\models\CNN  \
+    -c E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\configs\CNN\CNN.yaml
 
--r E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\feature_data -n TESS -o E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\models\KNN  -c E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\configs\KNN\KNN.yaml
+-r E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\feature_data -n TESS -o E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\models\CNN  -c E:\Courses\Recognition\Final_Project\Pattern_Recognition_Final_Project\configs\CNN\CNN.yaml
 '''
+
